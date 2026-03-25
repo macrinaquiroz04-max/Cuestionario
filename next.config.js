8fc920da3ae934c07f674ae0a533638c8617a36e
tachinloaa <tacho_lozada1972@live.com.mx>
2026-03-25 00:44:47 -0600

    fix: agregar dominio supabase.co a next/image remotePatterns

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '*.cloudflarestorage.com',
       },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
     ],
   },
 }
